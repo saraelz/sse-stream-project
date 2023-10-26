f4e6f95b12f9142be63badf2306d925ff1faa10e
Sara Elzeiny <saelzeiny@berkeley.edu>
2023-10-26 07:01:56 -0700

    Test cases

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-import json
-from flask import Flask, Response, jsonify
+from flask import Flask, jsonify
 import psycopg2  # Import the PostgreSQL library
 from psycopg2.extras import RealDictCursor
-
-app = Flask(__name__)
 
 
 def get_db_connection():
@@ -18,88 +15,89 @@ def get_db_connection():
     return conn
 
 
-@app.route('/')
-def index():
-    conn = get_db_connection()
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
-    cursor.execute('SELECT id, exam, studentid, score FROM scores;')
-    scores = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return jsonify({
-        'total_scores': len(scores),
-        'scores': scores
-    })
+def create_app():
+    app = Flask(__name__)
 
+    @app.route('/')
+    def index():
+        conn = get_db_connection()
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
+        cursor.execute('SELECT id, exam, studentid, score FROM scores;')
+        scores = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return jsonify({
+            'total_scores': len(scores),
+            'scores': scores
+        })
 
-@app.route('/users-with-scores')
-def users_with_scores():
-    """List all users that have received at least one test score"""
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT DISTINCT studentId FROM scores')
-    users = [row[0] for row in cursor.fetchall()]
-    cursor.close()
-    conn.close()
-    return jsonify(users)
+    @app.route('/users-with-scores')
+    def users_with_scores():
+        """List all users that have received at least one test score"""
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT DISTINCT studentId FROM scores')
+        users = [row[0] for row in cursor.fetchall()]
+        cursor.close()
+        conn.close()
+        return jsonify(users)
 
+    @app.route('/student/<student_id>')
+    def student_scores(student_id):
+        """List the test results for a specified student, and student's average score across all exams"""
+        conn = get_db_connection()
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
+        # Get test results for a specified student
+        cursor.execute(
+            'SELECT studentid, exam, score FROM scores WHERE studentId = %s', (student_id,))
+        student_results = cursor.fetchall()
 
-@app.route('/student/<student_id>')
-def student_scores(student_id):
-    """List the test results for a specified student, and student's average score across all exams"""
-    conn = get_db_connection()
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
-    # Get test results for a specified student
-    cursor.execute(
-        'SELECT studentid, exam, score FROM scores WHERE studentId = %s', (student_id,))
-    student_results = cursor.fetchall()
+        # Calculate the student's average score
+        total_score = sum(result['score'] for result in student_results)
+        average_score = total_score / \
+            len(student_results) if len(student_results) > 0 else 0
 
-    # Calculate the student's average score
-    total_score = sum(result['score'] for result in student_results)
-    average_score = total_score / \
-        len(student_results) if len(student_results) > 0 else 0
+        cursor.close()
+        conn.close()
+        return jsonify({
+            'student_results': student_results,
+            'average_score': average_score
+        })
 
-    cursor.close()
-    conn.close()
-    return jsonify({
-        'student_results': student_results,
-        'average_score': average_score
-    })
+    @app.route('/exams')
+    def exams():
+        """List all exams that have been recorded"""
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT DISTINCT exam FROM scores')
+        exams = [row[0] for row in cursor.fetchall()]
+        cursor.close()
+        conn.close()
+        return jsonify(exams)
 
+    @app.route('/exam/<exam_id>')
+    def exam_results(exam_id):
+        """List all the results for the specified exam, and average score across all students"""
+        conn = get_db_connection()
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
+        # List all results for the specified exam
+        cursor.execute(
+            'SELECT studentid, exam, score FROM scores WHERE exam = %s', (exam_id,))
+        exam_results = cursor.fetchall()
 
-@app.route('/exams')
-def exams():
-    """List all exams that have been recorded"""
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT DISTINCT exam FROM scores')
-    exams = [row[0] for row in cursor.fetchall()]
-    cursor.close()
-    conn.close()
-    return jsonify(exams)
-
-
-@app.route('/exam/<exam_id>')
-def exam_results(exam_id):
-    """List all the results for the specified exam, and average score across all students"""
-    conn = get_db_connection()
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
-    # List all results for the specified exam
-    cursor.execute(
-        'SELECT studentid, exam, score FROM scores WHERE exam = %s', (exam_id,))
-    exam_results = cursor.fetchall()
-
-    # Calculate the average score across all students for the exam
-    total_score = sum(result["score"] for result in exam_results)
-    average_score = total_score / \
-        len(exam_results) if len(exam_results) > 0 else 0
-    cursor.close()
-    conn.close()
-    return jsonify({
-        'exam_results': exam_results,
-        'average_score': average_score
-    })
+        # Calculate the average score across all students for the exam
+        total_score = sum(result["score"] for result in exam_results)
+        average_score = total_score / \
+            len(exam_results) if len(exam_results) > 0 else 0
+        cursor.close()
+        conn.close()
+        return jsonify({
+            'exam_results': exam_results,
+            'average_score': average_score
+        })
+    return app
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -1,42 +1,42 @@
 import json
 import pytest
-from app import app, get_db_connection
+import requests
+import sseclient
+from app import create_app, get_db_connection
+from listener import RawEvent
 
 
-def test_db_connection():
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config['TESTING'] = True
+    yield app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def db_connection():
     # Call the get_db_connection function
     conn = get_db_connection()
 
     # Check that the returned connection is not None
     assert conn is not None
-
-
-@pytest.fixture
-def client():
-    """Create a test client for the Flask app."""
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
+    return conn
 
 
 def test_index(client):
-    """Test the / endpoint."""
+    """Test the / endpoint.
+    If this fails, try to run listener.py"""
     response = client.get('/')
     assert response.status_code == 200
     data = json.loads(response.data.decode('utf-8'))
     assert 'total_scores' in data
     assert 'scores' in data
-
-
-def test_scores_database_populated(client):
-    """Returns true if scores database has at least one entry.
-    If this fails, try to run listener.py"""
-    response = client.get('/')
-    if response.status_code == 200:
-        data = json.loads(response.data.decode('utf-8'))
-        print('entries: ', data['total_scores'])
-        assert data['total_scores'] > 0
-    assert True
+    print('/', data)
 
 
 def test_users_with_scores(client):
@@ -47,13 +47,18 @@ def test_users_with_scores(client):
     assert isinstance(data, list)
 
 
-def test_student_scores(client):
+def test_student_scores(client, db_connection):
     """Test the /student/<student_id> endpoint."""
     # First, get a list of current student ids
-    user_response = client.get('/users-with-scores')
-    users_list = json.loads(user_response.data.decode('utf-8'))
+    cursor = db_connection.cursor()
+    row = None
+    while not row:
+        cursor.execute('SELECT studentId FROM scores;')
+        row = cursor.fetchone()
+    cursor.close()
+    db_connection.close()
     # Use with a valid student ID from your test data
-    student_id = users_list[0]
+    student_id = row[0]
     response = client.get(f'/student/{student_id}')
     assert response.status_code == 200
     data = json.loads(response.data.decode('utf-8'))
@@ -69,18 +74,53 @@ def test_exams(client):
     assert isinstance(data, list)
 
 
-def test_exam_results(client):
+def test_exam_results(client, db_connection):
     """Test the /exam/<exam_id> endpoint."""
     # First, get a list of current exam ids
-    exams_response = client.get('/exams')
-    exams_list = json.loads(exams_response.data.decode('utf-8'))
+    cursor = db_connection.cursor()
+    row = None
+    while not row:
+        cursor.execute('SELECT exam FROM scores;')
+        row = cursor.fetchone()
+    cursor.close()
+    db_connection.close()
     # Use with a valid exam ID from your test data
-    exam_id = exams_list[0]
+    exam_id = row[0]
     response = client.get(f'/exam/{exam_id}')
     assert response.status_code == 200
     data = json.loads(response.data.decode('utf-8'))
     assert 'exam_results' in data
     assert 'average_score' in data
+
+
+def test_queue_is_represented(client):
+    n_events = 5
+    URL = 'https://live-test-scores.herokuapp.com/scores'
+    response = requests.get(URL, stream=True)
+    stream = sseclient.SSEClient(response)
+    i = 0
+    for event in stream.events():
+        raw_event = RawEvent(event.event, event.data)
+        # Use with a valid student ID from your test data
+        student = json.loads(raw_event.raw_data)
+        student_id = student['studentId']
+        # Check response of /student/student_id
+        response = client.get(f'/student/{student_id}')
+        assert response.status_code == 200
+        data = json.loads(response.data.decode('utf-8'))
+        assert 'student_results' in data
+        assert 'average_score' in data
+        # Check response of /exam/exam_id
+        exam_id = student['exam']
+        response = client.get(f'/exam/{exam_id}')
+        assert response.status_code == 200
+        data = json.loads(response.data.decode('utf-8'))
+        assert 'exam_results' in data
+        assert 'average_score' in data
+
+        i = i + 1
+        if i > n_events:
+            return True
 
 
 if __name__ == '__main__':
